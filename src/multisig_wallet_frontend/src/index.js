b90eb27ef4d51a8b60a673f4cef5c7e649c78a8b
timk11 <timk@email.com>
2023-06-18 01:22:27 +1000

    Initial commit.

---
+++ src/multisig_wallet_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { multisig_wallet_backend } from "../../declarations/multisig_wallet_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await multisig_wallet_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
